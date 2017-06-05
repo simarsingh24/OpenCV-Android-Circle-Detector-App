570831065b3684ae863feddf0966a53835413f87
Harsimar <simarsingh24@gmail.com>
2017-06-05 17:48:18 +0530

    added cameraKit

--- app/src/main/java/com/example/harsimar/opencvandroid_circledetector/MainActivity.java
+++ app/src/main/java/com/example/harsimar/opencvandroid_circledetector/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.harsimar.opencvandroid_circledetector;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
